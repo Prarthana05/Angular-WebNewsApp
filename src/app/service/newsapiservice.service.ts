737aa58b20188a0be5d907bac0c46913edcbdcd7
Prarthana <artikumari1974@gmail.com>
2021-12-24 13:04:57 +0530

    Updated County code

--- src/app/service/newsapiservice.service.ts
+++ src/app/service/newsapiservice.service.ts
@@ -12,11 +12,11 @@ export class NewsapiserviceService {
 
     //tech api
 
-    techApiUrl = "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=bb0b36ca920d4fb9a56e6c2103d7a7d9"
+    techApiUrl = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=bb0b36ca920d4fb9a56e6c2103d7a7d9"
 
     //business api
 
-    buisnessUrl="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bb0b36ca920d4fb9a56e6c2103d7a7d9"
+    buisnessUrl="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=bb0b36ca920d4fb9a56e6c2103d7a7d9"
     
   topHeading():Observable<any>{  
   return this._http.get(this.newsApiUrl)
